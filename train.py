697f74aafdf8944be64a81118ed7543ea65f8339
The School of AI <49971681+theschoolofai@users.noreply.github.com>
2023-06-27 12:38:04 +0530

    Update train.py

--- train.py
+++ train.py
@@ -6,6 +6,13 @@ train_acc = []
 test_acc = []
 lrs = []
 
+def get_lr(optimizer):
+    """"
+    for tracking how your learning rate is changing throughout training
+    """
+    for param_group in optimizer.param_groups:
+        return param_group['lr']
+      
 def train(model, device, train_loader, optimizer, epoch, scheduler, criterion):
   model.train()
   pbar = tqdm(train_loader)
