8a076c00850011b47aca78c11e1c43e40d13a74c
The School of AI <49971681+theschoolofai@users.noreply.github.com>
2023-06-27 12:03:51 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ from functools import reduce
 from typing import Union
 import torch
 from torch import nn
-
+from torchinfo import summary
 
 def show_grad_cam(
     model,
@@ -172,3 +172,22 @@ def show_lr_history(trainer, epochs):
     # fig.set_size_inches(30, 10)
     plt.tight_layout()
     plt.show()
+    
+def print_summary(model, device, input_size, batch_size=20):
+    model = model.to(device=device)
+    s = summary(
+        model,
+        input_size=(batch_size, *input_size),
+        verbose=0,
+        col_names=[
+            "kernel_size",
+            "input_size",
+            "output_size",
+            "num_params",
+            "mult_adds",
+            "trainable",
+        ],
+        row_settings=["var_names"],
+    )
+
+    print(s)
