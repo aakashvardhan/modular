1d6aba517f9190b30e917f3815f1b62456e59b4a
The School of AI <49971681+theschoolofai@users.noreply.github.com>
2023-06-27 12:28:31 +0530

    Update transforms.py

--- transforms.py
+++ transforms.py
@@ -1,47 +1,23 @@
-from albumentations import (
-    Compose,
-    Normalize,
-    RandomCrop,
-    PadIfNeeded,
-    CoarseDropout,
-    Sequential,
-    OneOf,
-    HorizontalFlip,
-)
-from albumentations.pytorch.transforms import ToTensorV2
-import numpy as np
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
 
 class CustomResnetTransforms:
-    def __init__(self, means, stds, train=True):
-        if train:
-            self.transformations = Compose(
-                [
-                    Normalize(mean=means, std=stds, always_apply=True),
-                    Sequential(
-                        [
-                            PadIfNeeded(
-                                min_height=40, min_width=40, always_apply=True
-                            ),  # padding of 4 on each side of 32x32 image
-                            RandomCrop(height=32, width=32, always_apply=True),
-                        ]
-                    ),
-                    HorizontalFlip(),
-                    CoarseDropout(
-                        max_height=8,
-                        max_width=8,
-                        min_height=8,
-                        min_width=8,
-                        min_holes=1,
-                        max_holes=1,
-                        fill_value=means,
-                    ),
-                    ToTensorV2(),
-                ]
-            )
-        else:
-            self.transformations = Compose(
-                [
-                    Normalize(mean=means, std=stds, always_apply=True),
-                    ToTensorV2(),
-                ]
-            )
+    def train_transforms(means, stds):
+        return A.Compose(
+            [
+                A.Normalize(mean=means, std=stds, always_apply=True),
+                A.PadIfNeeded(min_height=40, min_width=40, always_apply=True),
+                A.RandomCrop(height=32, width=32, always_apply=True),
+                A.HorizontalFlip(),
+                A.CoarseDropout(max_holes=1, max_height=8, max_width=8, min_holes=1, min_height=8, min_width=8, fill_value=means),
+                ToTensorV2(),
+            ]
+        )
+
+    def test_transforms(means, stds):
+        return A.Compose(
+            [
+                A.Normalize(mean=means, std=stds, always_apply=True),
+        ToTensorV2(),
+            ]
+        )
